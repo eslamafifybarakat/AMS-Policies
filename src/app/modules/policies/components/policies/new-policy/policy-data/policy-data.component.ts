2ae8751b0296231fd7988126f83eae307dad9eb7
Eslam Afify Barakat <52158422+eslamafifybarakat@users.noreply.github.com>
2022-10-12 23:12:56 +0200

    fixed

--- src/app/modules/policies/components/policies/new-policy/policy-data/policy-data.component.ts
+++ src/app/modules/policies/components/policies/new-policy/policy-data/policy-data.component.ts
@@ -1,5 +1,5 @@
 import { Subscription } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { SearchCountryField, CountryISO } from 'ngx-intl-tel-input';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -13,6 +13,10 @@ import { Location } from '@angular/common';
 export class PolicyDataComponent implements OnInit {
   private unsubscribe: Subscription[] = [];
   isloading: boolean = false;
+  isEdit: boolean = false;
+
+  passportImageFile: File[] = [];
+  isMaxImage: boolean = false;
 
   jobNames = ['Uber', 'Microsoft', 'Flexigen'];
 
@@ -31,7 +35,8 @@ export class PolicyDataComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private location: Location,
-    public router: Router
+    public router: Router,
+    private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
@@ -44,8 +49,12 @@ export class PolicyDataComponent implements OnInit {
     Validators.maxLength(20)]],
     start_date: ["", [Validators.required]],
     end_date: ["", [Validators.required]],
+    birthdate: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
     phone: ['', [Validators.required]],
+    duration: ['', [Validators.required]],
+    passport_image: [null, [Validators.required]],
+    duration_type: ['', [Validators.required]],
     job: [null, [Validators.required]],
     gender: [null, [Validators.required]],
     nationality: [null, Validators.required],
@@ -59,6 +68,24 @@ export class PolicyDataComponent implements OnInit {
   get formControls(): any {
     return this.policyForm.controls;
   }
+
+  passportImageFileChangeEvent(event: any): void {
+    for (var i = 0; i < event?.target?.files?.length; i++) {
+      if (event?.target?.files[i]?.size <= 5120 * 1024) {
+        this.isMaxImage = false;
+        this.passportImageFile.push(event?.target?.files[i]);
+      } else {
+        this.isMaxImage = true;
+      }
+    }
+  }
+  onRemovePassportImage(name: any): void {
+    this.passportImageFile = this.passportImageFile?.filter((value: any) => {
+      return value?.name !== name;
+    });
+    this.cdr.detectChanges();
+  }
+
   submit(): void {
     console.log(this.policyForm?.value);
     this.router.navigate(['/home/policies/checkout']);
