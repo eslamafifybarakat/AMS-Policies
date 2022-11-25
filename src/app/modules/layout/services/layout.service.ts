6758d8a8dd6d577fdd3973ad8bd498b09c9d3fc4
Yarahegazy502 <yarahegazy502@gmail.com>
2022-11-25 16:39:07 +0200

    layout

---
+++ src/app/modules/layout/services/layout.service.ts
@@ -0,0 +1,19 @@
+import { environment } from './../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+  apiUrl = environment.apiUrl;
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  profileData(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl + "/api/dashboard/admins/auth/profile", data);
+  }
+}
